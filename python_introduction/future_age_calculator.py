cc79a5c85ec0accfb4fd94b93477126e510cfd6d
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-07-11 16:20:48 +0300

    second commit future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,1 +1,5 @@
-input=("How old are you?")
+age = int(input("How old are you? "))
+curr_year = 2023
+future_year = 2050
+future_age = age + (future_year - curr_year)
+print("In ",future_year,"you will be ",future_age," years old")

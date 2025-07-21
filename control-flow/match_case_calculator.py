a21fa5fc75675f16ca3a5740f62329766ba83de4
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-07-21 14:31:51 +0300

    initial commit: match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,5 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+operation = int(input("Choose the operation (+, -, *, /): "))
+result = {first_number}{operation}{second_number}
+print(result)

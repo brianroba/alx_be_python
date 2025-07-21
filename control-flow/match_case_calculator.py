7974deb11eb9fc62e7ebc435fd93bc4dd6c82ecc
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-07-21 14:44:59 +0300

    fourth commit: match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,24 +4,24 @@ ops = int(input("Choose the operation (+, -, *, /): "))
 #result = {num1}{ops}{num2}
 #print(result)
 
-    match operation:
-        case '+':
-            result = num1 + num2
-            op_symbol = '+'
-        case '-':
-            result = num1 - num2
-            op_symbol = '-'
-        case '*':
-            result = num1 * num2
-            op_symbol = '*'
-        case '/':
-            if num2 == 0:
-                print("Error: Division by zero is undefined.")
-                return
-            result = num1 / num2
-            op_symbol = '/'
-        case _:
-            print("Invalid operation selected.")
+match operation:
+    case '+':
+        result = num1 + num2
+        op_symbol = '+'
+    case '-':
+        result = num1 - num2
+        op_symbol = '-'
+    case '*':
+        result = num1 * num2
+        op_symbol = '*'
+    case '/':
+        if num2 == 0:
+            print("Error: Division by zero is undefined.")
             return
+        result = num1 / num2
+        op_symbol = '/'
+    case _:
+        print("Invalid operation selected.")
+        return
             
  print(f"{num1} {op_symbol} {num2} = {result}")

b9b0d9a6732efbfbaacd4a8751a4803136b571cd
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-07-26 14:07:20 +0300

    initial commit: arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,4 @@
 def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Perform an arithmetic operation on two numbers.
-
-    Parameters:
-    - num1 (float): First number
-    - num2 (float): Second number
-    - operation (str): One of 'add', 'subtract', 'multiply', or 'divide'
-
-    Returns:
-    - float or str: Result of the operation or error message for division by zero
-    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':

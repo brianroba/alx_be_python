c53e67dd942edbc9e71567e51a5eab395db1cb25
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-08-02 14:08:44 +0300

    second commit: test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,11 +9,12 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):  # <-- rename from test_add to test_addition
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
         self.assertEqual(self.calc.add(0, 0), 0)
+
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
